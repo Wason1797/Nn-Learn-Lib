45cdcfb3bde5087e78eda11582c0bf0b90c01c00
wabrborich@hotmail.com <wabrborich@hotmail.com>
2018-12-31 00:42:29 -0500

    added docs explaining multilayer perceptron and the need for linear algebra

---
+++ nn_lib/Layers/nn.py
@@ -0,0 +1,6 @@
+
+class NeuralNetwork:
+    def __init__(self, _in_size, _hidden_size, _out_size):
+        self.in_size = _in_size
+        self.hidden_size = _hidden_size
+        self.out_size = _out_size
